1f0771de9c37119fe867559787cf14372887517b
Dmitry <example@gmail.com>
2021-08-30 21:42:43 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// 1. ТИПЫ ДАННЫХ
+// 1.1 Объекты
+
+let obj = {
+    name: 'John',
+    age: 23,
+    isMarried: false
+};
+
+// console.log(obj.isMarried);
+// console.log(obj['isMarried']);
+
+let arr = ['pic.jpg', 34, {a: 2, isTrue: false}, []];
+// console.log(arr[2]);
+
+// 2. ПРОСТОЕ ОБЩЕНИЕ С ПОЛЬЗОВАТЕЛЕМ
+// alert('Hello');
+
+// const responce = confirm('Are you ready?');
+// console.log(responce);
+
+// const responce = prompt('Have you already been to London?', "Yes");
+// console.log(responce);
+// console.log(typeof(responce));
+
+// const answers = [];
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('How old are you?', '');
+// answers[2] = prompt('Are you maried?', '');
+// // console.log(answers);
+// document.write(answers);
+
+// const url = 'https://someurl.com/';
+// console.log(`${url}toys`);
+
+const isChecked = true,
+    isClosed = true;
+
